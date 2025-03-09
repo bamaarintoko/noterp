dd9e2c9bb4c46db2e6865c6159386b7028fcaf2f
bama <yosafatbama.arintoko@gmail.com>
2025-03-09 11:33:50 +0700

    done

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,39 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
+// Define PWA configuration separately
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ['pagedone.io'], // Tambahkan hostname di sini
-  }
+const pwaConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
 };
 
-export default nextConfig;
+// Define Next.js configuration separately
+
+export default withPWA(pwaConfig)({
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "pagedone.io",
+          pathname: "/**", // Allow all TMDB image paths
+        },
+      ],
+    },
+    
+    reactStrictMode: false,
+    // experimental: {
+    //   appDir: true, // Enable App Router
+    // },
+  }) as NextConfig;
+
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ['pagedone.io'], // Tambahkan hostname di sini
+//   }
+// };
+
+// export default nextConfig;
